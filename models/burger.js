caa1cc508cb440399546672a1987e43561879784
Chris Armendarez <chris.michael.armo@gmail.com>
2018-09-30 22:28:55 -0700

    Modify burger model

--- models/burger.js
+++ models/burger.js
@@ -21,16 +21,16 @@ burger.selectAll = (cb) => {
 };
 
 // Select all burgers
-burger.insertOne = (values, cb) => {
-    orm.insertOne("burger", values, (err, data) => {
+burger.insertOne = (cols, values, cb) => {
+    orm.insertOne("burger", cols, values, (err, data) => {
         if (!err) cb(null, data);
         else cb(err);
     });
 };
 
 // Select all burgers
-burger.updateOne = (id, col, val, cb) => {
-    orm.updateOne("burger", id, col, val, (err, data) => {
+burger.updateOne = (id, colsVals, cb) => {
+    orm.updateOne("burger", id, colsVals, (err, data) => {
         if (!err) cb(null, data);
         else cb(err);
     });
