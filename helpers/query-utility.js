51c446b7aed795e84f02e6c3f3037034a26a05cb
Chris Armendarez <chris.michael.armo@gmail.com>
2018-09-30 22:27:22 -0700

    Fix question mark getters + add object to sql

--- helpers/query-utility.js
+++ helpers/query-utility.js
@@ -6,11 +6,31 @@
 // ----------------------------------------
 
 // Create question marks
-const getQuestionMarks = (values) => {
-    return values.map((value) => "?").join(", ");
+const getQuestionMarks = (items) => items.map((_) => "?").join(", ");
+
+// Create double question marks
+const getDoubleQuestionMarks = (items) => items.map((_) => "??").join(", ");
+
+// Create sql value string
+const objectToSql = (obj) => {
+    const sqlPairs = [];
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            let value = obj[key];
+            if (typeof value === "string" && value.indexOf(" ") > -1) {
+                value = `"${value}"`;
+            }
+            sqlPairs.push(`${key}=${value}`);
+        }
+    }
+    return sqlPairs.toString();
 };
 
 // Export
 // ----------------------------------------
 
-module.exports = getQuestionMarks;
+module.exports = {
+    getQuestionMarks,
+    getDoubleQuestionMarks,
+    objectToSql
+};
